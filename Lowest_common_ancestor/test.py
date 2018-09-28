eac9d28183c48ef85a797f4daaf9536ee7f6bb7e
Woodse07 <sewoods@tcd.ie>
2018-09-28 16:16:36 +0100

    Added some comments for clarity

--- Lowest_common_ancestor/test.py
+++ Lowest_common_ancestor/test.py
@@ -4,19 +4,29 @@ from LCA import lowest_common_ancestor
 
 class myTest(unittest.TestCase):
     def test_correct_lca(self):
+        #Creating graph with networkx library..
         G = nx.DiGraph()
+        #Adding random nodes..
         G.add_nodes_from(['a','b','b','d','e','f','g','h','i','j','k','l','m','n','o','p'])
+        #Adding random edges..
         G.add_edges_from([('a','b'),('a','c'),('b','d'),('b','e'),('c','f'),('c','g'),('d','h'),
                             ('d','j'),('e','k'),('e','l'),('f','m'),('g','o'),('h','i'),('m','n'),('o','p'),])
 
+        #General cases (Random nodes for testing)                           #       Graphic of the binary tree
         self.assertEqual(lowest_common_ancestor(G, 'p', 'i'), 'a')          #                  _a_
         self.assertEqual(lowest_common_ancestor(G, 'n', 'o'), 'c')          #                /    \__
         self.assertEqual(lowest_common_ancestor(G, 'b', 'c'), 'a')          #                b       c__
         self.assertEqual(lowest_common_ancestor(G, 'h', 'c'), 'a')          #              /  \      /  \
         self.assertEqual(lowest_common_ancestor(G, 'i', 'l'), 'b')          #             d   e     f    g
         self.assertEqual(lowest_common_ancestor(G, 'i', 'j'), 'd')          #           / \  /\    /     \
-        #self.assertEqual(lowest_common_ancestor(G, 'a', 'b'), 'a')         #          h  j k  l  m       o
-        #self.assertEqual(lowest_common_ancestor(G, 'a', 'a'), 'a')         #         /          /         \
+                                                                            #          h  j k  l  m       o
+                                                                            #         /          /         \
                                                                             #        i          n           p
+        #Special cases (if one of the nodes is root, the LCA is the root)
+        self.assertEqual(lowest_common_ancestor(G, 'a', 'b'), 'a')
+        #LCA of nodes x and x is x
+        self.assertEqual(lowest_common_ancestor(G, 'a', 'a'), 'a') 
+        self.assertEqual(lowest_common_ancestor(G, 'd', 'd'), 'd')
+
 if __name__ == '__main__':
     unittest.main()
