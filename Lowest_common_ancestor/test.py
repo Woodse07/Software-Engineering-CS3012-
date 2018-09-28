454b57927f09e201c35e7e309ce105c96e0f7278
Woodse07 <sewoods@tcd.ie>
2018-09-28 14:32:17 +0100

    Attempted a graphic of what the binary tree looks like

--- Lowest_common_ancestor/test.py
+++ Lowest_common_ancestor/test.py
@@ -9,14 +9,14 @@ class myTest(unittest.TestCase):
         G.add_edges_from([('a','b'),('a','c'),('b','d'),('b','e'),('c','f'),('c','g'),('d','h'),
                             ('d','j'),('e','k'),('e','l'),('f','m'),('g','o'),('h','i'),('m','n'),('o','p'),])
 
-        self.assertEqual(lowest_common_ancestor(G, 'p', 'i'), 'a')
-        self.assertEqual(lowest_common_ancestor(G, 'n', 'o'), 'c')
-        self.assertEqual(lowest_common_ancestor(G, 'b', 'c'), 'a')
-        self.assertEqual(lowest_common_ancestor(G, 'h', 'c'), 'a')
-        self.assertEqual(lowest_common_ancestor(G, 'i', 'l'), 'b')
-        self.assertEqual(lowest_common_ancestor(G, 'i', 'j'), 'd')
-        #self.assertEqual(lowest_common_ancestor(G, 'a', 'b'), 'a')
-        #self.assertEqual(lowest_common_ancestor(G, 'a', 'a'), 'a')
-
+        self.assertEqual(lowest_common_ancestor(G, 'p', 'i'), 'a')          #                  _a_
+        self.assertEqual(lowest_common_ancestor(G, 'n', 'o'), 'c')          #                /    \__
+        self.assertEqual(lowest_common_ancestor(G, 'b', 'c'), 'a')          #                b       c__
+        self.assertEqual(lowest_common_ancestor(G, 'h', 'c'), 'a')          #              /  \      /  \
+        self.assertEqual(lowest_common_ancestor(G, 'i', 'l'), 'b')          #             d   e     f    g
+        self.assertEqual(lowest_common_ancestor(G, 'i', 'j'), 'd')          #           / \  /\    /     \
+        #self.assertEqual(lowest_common_ancestor(G, 'a', 'b'), 'a')         #          h  j k  l  m       o
+        #self.assertEqual(lowest_common_ancestor(G, 'a', 'a'), 'a')         #         /          /         \
+                                                                            #        i          n           p
 if __name__ == '__main__':
     unittest.main()
