c1fa2225e44cf7567857c4c187c8ae4d638ffca3
Woodse07 <sewoods@tcd.ie>
2018-09-28 13:43:08 +0100

    Code for lowest common ancestor function should be done now

--- Lowest_common_ancestor/LCA.py
+++ Lowest_common_ancestor/LCA.py
@@ -2,22 +2,38 @@ import networkx as nx
 
 #Start functions
 def lowest_common_ancestor(graph, a, b):
-    print("here")
-    holdA = ''
-    holdB = ''
-    countA = 0
-    countB = 0
-    i = 0
+    lca = None
 
-    holdA = a
-    while(holdA != None):
-        countA += 1
-        holdA = (G.predecessors(holdA)).next()
+    node_a_parents = set()
+    finished = 0
+    temp = a
+    while(not finished):
+        if(len(list(graph.predecessors(temp))) != 0):
+            node = graph.predecessors(temp).next()
+            node_a_parents.add(node)
+            temp = node
+        else:
+            finished = 1
+    print("Parents of node A: " + str(node_a_parents))
 
-    holdB = b
-    while(holdB != None):
-        countB += 1
-        holdB = (G.predecessors(holdB)).next()
+    node_b_parents = set()
+    finished = 0
+    temp = b
+    while(not finished):
+        if(len(list(graph.predecessors(temp))) != 0):
+            node = graph.predecessors(temp).next()
+            node_b_parents.add(node)
+            temp = node
+        else:
+            finished = 1
+    print("Parents of node B: " + str(node_b_parents))
+
+    #Find first intersection in set #B
+    intersec = node_a_parents.intersection(node_b_parents)
+    while(bool(intersec)):
+        lca = intersec.pop()
+
+    return lca
 #End functions
 
 
@@ -29,4 +45,4 @@ G.add_edges_from([('a','b'),('a','c'),('b','d'),('b','e'),('c','f'),('c','g'),('
 print ("Nodes: " + str(G.nodes))
 print ("Edges: " + str(G.edges))
 
-lowest_common_ancestor(G, 'p', 'i')
+print(lowest_common_ancestor(G, 'h', 'l'))
