9f4f46f7327e2511e676ebeeb103580d7da59139
Woodse07 <sewoods@tcd.ie>
2018-09-28 13:55:44 +0100

    Beginning to write tests + cleaned up some things

--- Lowest_common_ancestor/LCA.py
+++ Lowest_common_ancestor/LCA.py
@@ -33,16 +33,6 @@ def lowest_common_ancestor(graph, a, b):
     while(bool(intersec)):
         lca = intersec.pop()
 
+    print("Lowest common ancestor of " + a + " and " + b + " is: " + str(lca) + "!")
     return lca
 #End functions
-
-
-G = nx.DiGraph()
-G.add_nodes_from(['a','b','b','d','e','f','g','h','i','j','k','l','m','n','o','p'])
-G.add_edges_from([('a','b'),('a','c'),('b','d'),('b','e'),('c','f'),('c','g'),('d','h'),
-                    ('d','j'),('e','k'),('e','l'),('f','m'),('g','o'),('h','i'),('m','n'),('o','p'),])
-
-print ("Nodes: " + str(G.nodes))
-print ("Edges: " + str(G.edges))
-
-print(lowest_common_ancestor(G, 'h', 'l'))
--- Lowest_common_ancestor/test.py
+++ Lowest_common_ancestor/test.py
@@ -1,3 +1,15 @@
 import unittest
+import networkx as nx
+from LCA import lowest_common_ancestor
 
-#unit test file
+class myTest(unittest.TestCase):
+    def test(self):
+        G = nx.DiGraph()
+        G.add_nodes_from(['a','b','b','d','e','f','g','h','i','j','k','l','m','n','o','p'])
+        G.add_edges_from([('a','b'),('a','c'),('b','d'),('b','e'),('c','f'),('c','g'),('d','h'),
+                            ('d','j'),('e','k'),('e','l'),('f','m'),('g','o'),('h','i'),('m','n'),('o','p'),])
+
+        self.assertEqual(lowest_common_ancestor(G, 'p', 'i'), 'a')
+
+if __name__ == '__main__':
+    unittest.main()
