b21f894a75047d511fcbed9637f64ed1aa8b984f
Woodse07 <sewoods@tcd.ie>
2018-10-05 13:37:46 +0100

    Added insert function to binary tree data structure

--- Lowest_common_ancestor/LCA_binary_tree.py
+++ Lowest_common_ancestor/LCA_binary_tree.py
@@ -5,12 +5,36 @@ class Node:
         self.left = None
         self.right = None
 
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+
     def printTree(self):
         if self.left:
             self.left.printTree()
-        print(self.data)
+        print(self.data),
         if self.right:
             self.right.printTree()
 
+# Function definintions end here
+
+
 root = Node(10)
+root.insert(5)
+root.insert(7)
+root.insert(15)
+root.insert(12)
 root.printTree()
