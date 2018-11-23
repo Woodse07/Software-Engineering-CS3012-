f3e0d29d69eb3bf7998b5d57678dbef0737ca47f
Woodse07 <sewoods@tcd.ie>
2018-11-23 14:42:10 +0000

    Have a very primitive version working on command line

--- Social_Graph/scraper.py
+++ Social_Graph/scraper.py
@@ -1,32 +1,62 @@
 from github import Github
+import getpass
+import networkx as nx
 
+
+def find_match(followers, user, goal):
+
+	queue = []
+	queue.append([user.login])
+	while queue:
+		path = queue.pop(0)
+		node = path[-1]
+		print(node)
+		if node == goal.login:
+			return path
+		for follower in g.get_user(node).get_followers():
+			new_path = list(path)
+			new_path.append(follower.login)
+			queue.append(new_path)
+
+
+#LOGGING IN THE USER
 Username = raw_input("Github Username: ")
-Password = raw_input("Github Password: ")
+Password = getpass.getpass("Github Password: ")
 g = Github(Username, Password) #(Username, password) or (Authentication_token) goes here
-
 user = g.get_user()
 user.login
 print("Successfully logged in user " + user.name)
 print("")
-print(user.name + " has the following repositories: ")
-for repo in g.get_user().get_repos():
-	print(repo.name)
+
+
+#FINDING PERSON OF INTEREST
+person_of_interest = raw_input("Who do you want to look for? ")
+print("Looking for " + person_of_interest + "...")
+poi = g.get_user(person_of_interest)
+if poi is None:
+	print(person_of_interest + " does not exist!")
+print("Found: " + poi.name)
 print("")
-repo = g.get_repo("Woodse07/Software-Engineering-CS3012-")
-print("Contents of " + user.name + "'s Software Engineering Repo: ")
-for contents in repo.get_contents(""):
-	print(contents)
-print("")
-print("Branches of " + user.name + "'s Software Engineering Repo: ")
-for branch in repo.get_branches():
-	print(branch)
-print("")
-print("Authors and Dates of all commits of " + user.name + "'s Software Engineering Repo: ")
-count = 0
-for commit in repo.get_commits():
-	print(commit.commit.author)
-	print(commit.commit.author.date)
-	print("")
-	count += 1
-print("Total amount of commits: " + str(count))
+
+
+#CONSTRUCTING NETWORK FOR USER 3 LEVELS DEEP
+G = nx.DiGraph()
+
+#FINDING LINK BETWEEN USER AND PERSON OF INTEREST
+followers = user.get_followers()
+found = 0
+dos = 0
+r = find_match(followers, user, poi)
+if r is not 0:
+	print("Found a match!")
+	print("Degree of seperation: " + str(len(r)-1))
+	print("Path from " + user.name + " to " + poi.name + ": ")
+	path = ""
+	for element in r:
+		path += element + " -> "
+	print(path[:-4])
+			
+		
+
+
 
