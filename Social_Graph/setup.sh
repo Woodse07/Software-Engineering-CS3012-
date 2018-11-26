6de26f0afc90b0ff6c801b2722b2c5043dce6cb5
Woodse07 <sewoods@tcd.ie>
2018-11-26 12:18:46 +0000

    Fixed small typo

--- Social_Graph/setup.sh
+++ Social_Graph/setup.sh
@@ -4,7 +4,7 @@
 echo "Calling python script to generate Network..."
 python network_generator.py
 echo "Converting our gml files to json..."
-python networks/gml2json_cli.py smallnetwork.gml smallnetwork.json
-python networks/gml2json_cli.py med_network.gml med_network.json
+python networks/gml2json_cli.py networks/smallnetwork.gml smallnetwork.json
+python networks/gml2json_cli.py networks/med_network.gml med_network.json
 echo "Spinning up Flask..."
 python start.py
