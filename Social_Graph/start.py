660bad4a42274e654c13f1124ce23d9e9fa3e5e2
Woodse07 <sewoods@tcd.ie>
2018-11-28 12:45:53 +0000

    Added comments for Flask file

--- Social_Graph/start.py
+++ Social_Graph/start.py
@@ -12,6 +12,9 @@ app = Flask(__name__)
 def get_path():
 	return render_template("index.html")
 
+# Endpoint for getting a match, takes in the Username of person of interest.
+# Calls find_match.py, whose biproduct is a gml file of the path.
+# Calls gml2json with the biproduct of find_match and retruns the JSON.
 @app.route('/get_match', methods=['POST'])
 def get_path_post():
 	name = request.args.get("name")
@@ -19,10 +22,13 @@ def get_path_post():
 	print(find_match.findMatch(Username, Password, name))
 	return gml2json.main("temp.gml")
 
+# Endpoint for getting the users network.
+# Simply returns the json of their network which was pre constructed on setup.
 @app.route('/get_network/', methods=['GET'])
 def get_network_post():
 	return send_file("networks/smallnetwork.json")
 
+# Asks for username and password of github account on start up.
 if __name__ == '__main__':
 	Username = raw_input("Github Username: ")
 	Password = getpass.getpass("Github Password: ")
